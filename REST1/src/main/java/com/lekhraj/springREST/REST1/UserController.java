e9a38e12b8abf5155c44650111d5c9e89f256b66
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-01-05 23:01:21 -0800

    Send URI od newly added user and Response  code as 201 not 200.
    201 stands for created

--- REST1/src/main/java/com/lekhraj/springREST/REST1/UserController.java
+++ REST1/src/main/java/com/lekhraj/springREST/REST1/UserController.java
@@ -1,13 +1,16 @@
 package com.lekhraj.springREST.REST1;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -28,7 +31,11 @@ public class UserController {
 	//Add New User
 	@PostMapping("/users")
 	public void save(@RequestBody User u) {
-		 service.save(u);
+		 User savedUser = service.save(u);
+		 
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(savedUser.getId()).toUri();
+		 ResponseEntity.created(uri); //It will send response code as 201:Created.
+		 
 		//return service.save(u);
 	}
 

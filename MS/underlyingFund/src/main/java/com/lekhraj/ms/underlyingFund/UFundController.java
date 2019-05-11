bf57c8dceeb270780811ee07899b0288018bf9a2
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-05-11 15:23:37 -0700

    dynamic port
    org.springframework.core.env.Environment;

--- MS/underlyingFund/src/main/java/com/lekhraj/ms/underlyingFund/UFundController.java
+++ MS/underlyingFund/src/main/java/com/lekhraj/ms/underlyingFund/UFundController.java
@@ -1,15 +1,20 @@
 package com.lekhraj.ms.underlyingFund;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UFundController {
+	
+	@Autowired
+	Environment env;
 
 	@GetMapping
 	public UFund getHardcodedUFund() {
 		
-		return new UFund("NPF", "TDF10", true, "dev-1", 8000);
+		return new UFund("NPF", "TDF10", true, "dev-1", Integer.parseInt(env.getProperty("local.server.port")));
 		
 	}
 }

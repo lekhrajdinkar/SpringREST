fc8232ae07ec8c9e82a9fda5d01732c37616b07d
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-05-12 17:12:16 -0700

    register target service with Eureka naming service

--- MS/target/src/main/java/com/lekhraj/ms/target/TargetApplication.java
+++ MS/target/src/main/java/com/lekhraj/ms/target/TargetApplication.java
@@ -2,8 +2,12 @@ package com.lekhraj.ms.target;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
+// Step 26 - Enable Eureka naming service
+@EnableDiscoveryClient
 public class TargetApplication {
 
 	public static void main(String[] args) {

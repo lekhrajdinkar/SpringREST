1de3541585cd51daf9175a947d02266f58efd846
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-05-12 17:46:20 -0700

    Adding note - imp

--- MS/underlyingFund/src/main/java/com/lekhraj/ms/underlyingFund/feign/TargetServiceProxy.java
+++ MS/underlyingFund/src/main/java/com/lekhraj/ms/underlyingFund/feign/TargetServiceProxy.java
@@ -9,7 +9,11 @@ import com.lekhraj.ms.underlyingFund.model.Target;
 //1st connecting to localhost:8100 instance
 //@FeignClient(name="target-service" , url="localhost:8100")
 
-//2nd - connecting tom multiple instance of same ms
+//2nd - connecting to multiple instance of same ms. Add Ribbon on top of feign with urls of target-service
+// 2.1. urls are added/hardcoded in prop files
+// 2.2. Register target-service with eureka naming server. Ribbon will automatically talk to it and get the urls.
+// NOTE: CHECK SERVICE NAME IN Eureka, put same name below
+
 @FeignClient(name="target-service")
 @RibbonClient(name="target-service")
 public interface TargetServiceProxy {

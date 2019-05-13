681b3e7c10bbedb55a81fc8b4f18ad3da5b0d82a
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2019-05-12 23:12:04 -0700

    enable Zuul and register with Eureka

--- MS/zuul-api-gateway/src/main/java/com/lekhraj/ms/zuulapigateway/ZuulApiGatewayApplication.java
+++ MS/zuul-api-gateway/src/main/java/com/lekhraj/ms/zuulapigateway/ZuulApiGatewayApplication.java
@@ -2,8 +2,15 @@ package com.lekhraj.ms.zuulapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+//Step 30: Enable Zuul
+@EnableZuulProxy
+
+//Register with Eureka
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ZuulApiGatewayApplication {
 
 	public static void main(String[] args) {
